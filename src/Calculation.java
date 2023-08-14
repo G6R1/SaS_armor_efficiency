a1410b11245caf779911d677ee3d93a65712601c
unknown <Gerian109@yandex.ru>
2023-08-14 21:39:34 +0300

    fix

--- src/Calculation.java
+++ src/Calculation.java
@@ -98,41 +98,5 @@ public class Calculation {
 
         return 1 - blockFailedCount / 10;
     }
-
-    public static class Armor {
-        private String name;
-        private int movementDice;
-        private int armor;
-
-        public Armor(String name, int movementDice, int armor) {
-            this.name = name;
-            this.movementDice = movementDice;
-            this.armor = armor;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getMovementDice() {
-            return movementDice;
-        }
-
-        public void setMovementDice(int movementDice) {
-            this.movementDice = movementDice;
-        }
-
-        public int getArmor() {
-            return armor;
-        }
-
-        public void setArmor(int armor) {
-            this.armor = armor;
-        }
-    }
 }
 
